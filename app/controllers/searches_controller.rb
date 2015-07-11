b01138c023e97b95549fac7736ffc4510a636bb7
Danelson Rosa <danelson.rosa@gmail.com>
2015-07-11 11:26:24 -0400

    Minor search form placeholder update.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,6 @@
 class SearchesController < ApplicationController
   def new
+    @random_word_placeholder = "e.g. #{Word.all.sample.name}"
     @query = params[:search]
     if @query
       @search_results = Word.search(@query)
