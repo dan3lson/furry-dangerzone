4d35a60a6a0d95273501ac68b1f5aa04b9a221d2
Danelson Rosa <danelson.rosa@gmail.com>
2015-07-31 14:18:17 -0400

    Updated seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,22 @@ Rails.logger.info "==============================================="
 Rails.logger.info "Creating Users"
 Rails.logger.info "==============================================="
 
-# @founder = User.create!(
-#   username: "dan3lson",
-#   password: "danelson",
-#   password_confirmation: "danelson",
-#   role: "admin"
-# )
-# Rails.logger.info "Username: #{@founder.username}"
+@founder = User.create!(
+  username: "dan3lson",
+  password: "danelson",
+  password_confirmation: "danelson",
+  role: "admin"
+)
+Rails.logger.info "Username: #{@founder.username}"
 
-# 2.times do
-#   user = User.create!(
-#     username: Faker::Internet.user_name,
-#     password: "password",
-#     password_confirmation: "password"
-#   )
-#   Rails.logger.info "Username: #{user.username}"
-# end
+2.times do
+  user = User.create!(
+    username: Faker::Internet.user_name,
+    password: "password",
+    password_confirmation: "password"
+  )
+  Rails.logger.info "Username: #{user.username}"
+end
 
 Rails.logger.info "==============================================="
 Rails.logger.info "Creating Levels"
@@ -92,3 +92,9 @@ Level.all.each do |level|
     level: level
   )
 end
+
+Rails.logger.info "==============================================="
+Rails.logger.info "Creating Version"
+Rails.logger.info "==============================================="
+
+Version.create!(number: "1.1.0", description: "* Making\n * Moves")
