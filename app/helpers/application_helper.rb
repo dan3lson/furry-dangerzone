3eea281fec212cf2fe52c790fb5731e9aecaf0dd
Danelson Rosa <danelson.rosa@gmail.com>
2015-08-20 11:10:16 -0500

    Redos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,7 +27,7 @@ module ApplicationHelper
 
   def tags_for_a(user, word)
     user.user_word_tags.map do |uwt|
-      uwt.word_tag.tag
+      uwt.word_tag.tag if uwt.word_tag.word == word
     end.compact
   end
 
