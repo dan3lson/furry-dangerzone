f6384803bb87781522d66e7bff5669e6bf64c449
Danelson Rosa <danelson.rosa@gmail.com>
2015-09-07 23:44:07 -0400

    Updates end of Fundamentals game actions.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,10 @@ class GamesController < ApplicationController
     @synonyms = @word.synonyms if @word.has_synonyms?
     @antonyms = @word.antonyms if @word.has_antonyms?
     @real_world_examples = RealWorldExample.provide(@word.name)
+    
+    if current_user.has_words? && current_user.has_incomplete_fundamentals?
+      @rand_word_id = current_user.incomplete_fundamentals.sample.word.id
+    end
   end
 
   def jeopardy
