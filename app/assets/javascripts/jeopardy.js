b924d5bbb4a418b4d925f5cf8c892ab3159bfc36
Danelson Rosa <danelson.rosa@gmail.com>
2015-09-20 23:57:49 -0400

    WIP Jeopardy integration: now displays words via js. Next are instructions and gameplay.

--- app/assets/javascripts/jeopardy.js
+++ app/assets/javascripts/jeopardy.js
@@ -4,31 +4,15 @@ $(document).ready(function(){
 	 * Initiating variables and arrays
 	 */
 
-	// General
-	var $chosen_word_value;
-	var $each_letter_span; // letters shown in the header
-	var $regex = /^[a-zA-Z. ]+$/; // No numbers or special characters just letters and spaces; /^[a-zA-Z]*$/; is only letters and no spaces or numbers
-	var $first_word = $("#chosen_word_one").val();
-	var $second_word = $("#chosen_word_two").val();
-	var $third_word = $("#chosen_word_three").val();
-	var $fourth_word = $("#chosen_word_four").val();
+	var $chosen_word_id = $(".hidden").text();
 	var $chosen_words_array = ["#chosen_word_one_div","#chosen_word_two_div","#chosen_word_three_div","#chosen_word_four_div"];
 	var $words_array_five_each = [];
-	var $question_type_array = ["synonym", "antonym", "sentence", "meaning"];
+	var $question_type_array = ["example_sentence", "definition"];
 	var $current_word_counter = 0;
 	var $correct_word_proggress_bar_without_pbc_class = "";
 
-	// Start the first activity
+	// Start the main activity
 	start_level_2();
-
-	/**
-	 * Handle the back buttons
-	 */
-
-	// If the user wants to start over with a different word
-	$("#restart_back_button").click(function(){
-		location.reload();
-	})
 
 	/**
 	 * Starter Functions: Main ones that initiate each activity
@@ -36,137 +20,137 @@ $(document).ready(function(){
 
 	// Start level 2 activity
 	function start_level_2() {
-		// After four words are typed, update the button to display the text
-		update_button_text("#chosen_word_one_btn","#chosen_word_one");
-		update_button_text("#chosen_word_two_btn","#chosen_word_two");
-		update_button_text("#chosen_word_three_btn","#chosen_word_three");
-		update_button_text("#chosen_word_four_btn","#chosen_word_four");
-
-		// Have a simple fade effect for the buttons when they're first displayed
-		for (var i = 0; i < 4; i++) {
-			$($chosen_words_array[i]).fadeIn();
-		}
-
-		// Create an array that holds four instances of the word/button names
-		for (var i = 0; i < 5; i++) {
-			words_array_five_each_push("#chosen_word_one");
-			words_array_five_each_push("#chosen_word_two");
-			words_array_five_each_push("#chosen_word_three");
-			words_array_five_each_push("#chosen_word_four");
-		}
-
-		// Shuffle the array so the order of word-questions is random
-		shuffle_array($words_array_five_each);
-
-		// Randomly select what question-type to display, i.e. syn, ant, sent, TBD, TBD
-		shuffle_array($question_type_array);
-
-		// Read the XML file
+		// Get the details of all words
 		$.ajax({
 			type: "GET",
-			url: "dictionary.xml",
-			dataType: "xml",
-			success: function (xml) {
-				console.log("Current word in the 20-word array (before clicked):",$words_array_five_each[$current_word_counter]);
-				console.log("Randomly chosen question type (before clicked):",$question_type_array[$current_word_counter]);
-				console.log("Question Types (before click):",$question_type_array);
+			url: "/jeopardy_game_words?word_id=" + $chosen_word_id,
+			dataType: "json",
+			success: function (response) {
+				var $chosen_word = response[0]
+				var $second_word = response[1]
+				var $third_word = response[2]
+				var $fourth_word = response[3]
+				// Display the text for the remaining three buttons
+				update_button_text("#chosen_word_one_btn", $chosen_word);
+				update_button_text("#chosen_word_two_btn", $second_word);
+				update_button_text("#chosen_word_three_btn", $third_word);
+				update_button_text("#chosen_word_four_btn", $fourth_word);
+
+				// Create an array that holds four instances of the word/button names
+				for (var i = 0; i < 5; i++) {
+					words_array_five_each_push($chosen_word);
+					words_array_five_each_push($second_word);
+					words_array_five_each_push($third_word);
+					words_array_five_each_push($fourth_word);
+				}
+
+				// Shuffle the array so the order of word-questions is random
+				shuffle_array($words_array_five_each);
+
+				// Randomly select what question-type to display, i.e. syn, ant, sent, TBD, TBD
+				shuffle_array($question_type_array);
+
+				console.log("Current word in the 20-word array (before clicked):", $words_array_five_each[$current_word_counter].name);
+				console.log("Randomly chosen question type (before clicked):", $question_type_array[$current_word_counter]);
+				console.log("Question Types (before click):", $question_type_array);
 				console.log("****************");
 
-				$(xml).find($words_array_five_each[$current_word_counter]).children().find($question_type_array[0]).each(function(){
+				var $current_word = $words_array_five_each[$current_word_counter]
+
+				console.log("Question type:", $question_type_array[0]);
+				display_activity_instruction($question_type_array[0], $current_word.data($question_type_array[0]));
+
+				$.find($words_array_five_each[$current_word_counter]).children().find($question_type_array[0]).each(function(){
 					// Update the activity name and instruction
 					// This only displays the last item, which is OKAY for now... =D =/
-					display_activity_instruction($question_type_array[0], $(this).text());
 				});
-
-				answer_the_question("#chosen_word_one_btn","#chosen_word_one_progress_bars_div .progress-bar-custom:first");
-				answer_the_question("#chosen_word_two_btn","#chosen_word_two_progress_bars_div .progress-bar-custom:first");
-				answer_the_question("#chosen_word_three_btn","#chosen_word_three_progress_bars_div .progress-bar-custom:first");
-				answer_the_question("#chosen_word_four_btn","#chosen_word_four_progress_bars_div .progress-bar-custom:first");
-
-				// answer_the_question("#chosen_word_one_btn","#chosen_word_one_progress_bars_div div:not(.progress-bar-success):first");
-				// answer_the_question("#chosen_word_two_btn","#chosen_word_two_progress_bars_div div:not(.progress-bar-success):first");
-				// answer_the_question("#chosen_word_three_btn","#chosen_word_three_progress_bars_div div:not(.progress-bar-success):first");
-				// answer_the_question("#chosen_word_four_btn","#chosen_word_four_progress_bars_div div:not(.progress-bar-success):first");
-
-				// Handles what happens when user answers correctly and incorrectly
-				function answer_the_question(button_id_name, first_mini_progress_bar_without_pbc_class) {
-					$(button_id_name).click(function(){
-						if ($(this).text() == $words_array_five_each[$current_word_counter]) {
-							// Update the nth mini progress bar for that word
-							$(first_mini_progress_bar_without_pbc_class).removeClass("progress-bar-custom").addClass("progress-bar-success");
-						}
-
-						// Find the id of the correct word to update the nth progress bar as incorrect /
-						if ($(this).text() != $words_array_five_each[$current_word_counter]) {
-							$correct_word_proggress_bar_without_pbc_class = "#" +
-									$("button:contains('"+$words_array_five_each[$current_word_counter]+"')")
-									.next()
-									.attr("id")
-									+ " .progress-bar-custom:first"
-								;
-							$($correct_word_proggress_bar_without_pbc_class).removeClass("progress-bar-custom").addClass("progress-bar-danger");
-						}
-
-						// Total up the goodies
-						get_goodies();
-
-						// Shuffle the question types
-						shuffle_array($question_type_array);
-						// Increase the counter
-						$current_word_counter++;
-						console.log("Current word in the 20-word array (after clicked):",$words_array_five_each[$current_word_counter]);
-						console.log("Randomly chosen question type (after clicked):",$question_type_array[0]);
-						console.log("Question Types (after clicked):",$question_type_array[0]);
-						console.log("Counter (after btn is clicked):",$current_word_counter);
-						console.log("****************");
-						display_activity_instruction(
-							$question_type_array[0],
-							$(xml).find($words_array_five_each[$current_word_counter]).children().find($question_type_array[0]+":first").text()
-						);
-
-						// When the game is over, display "complete" message
-						if ($current_word_counter == 20) {
-							$("#chosen_word_one_btn").prop("disabled",true);
-							$("#chosen_word_two_btn").prop("disabled",true);
-							$("#chosen_word_three_btn").prop("disabled",true);
-							$("#chosen_word_four_btn").prop("disabled",true);
-							display_activity_instruction("Ready for the results?", "3...2...1...");
-							setTimeout(function(){
-								$("#level_2_container").hide();
-								$("#review_level_two_container").show();
-
-								display_level_2_results(
-									"#chosen_word_one_progress_bars_div",
-									"#level_two_results_first_circle",
-									"#level_two_results_first_word",
-									$first_word
-								);
-
-								display_level_2_results(
-									"#chosen_word_two_progress_bars_div",
-									"#level_two_results_second_circle",
-									"#level_two_results_second_word",
-									$second_word
-								);
-
-								display_level_2_results(
-									"#chosen_word_three_progress_bars_div",
-									"#level_two_results_third_circle",
-									"#level_two_results_third_word",
-									$third_word
-								);
-
-								display_level_2_results(
-									"#chosen_word_four_progress_bars_div",
-									"#level_two_results_fourth_circle",
-									"#level_two_results_fourth_word",
-									$fourth_word
-								);
-
-							}, 3000);
-						}
-					});
-				}; // end of answer the question fn
+			//
+			// 	answer_the_question("#chosen_word_one_btn","#chosen_word_one_progress_bars_div .progress-bar-custom:first");
+			// 	answer_the_question("#chosen_word_two_btn","#chosen_word_two_progress_bars_div .progress-bar-custom:first");
+			// 	answer_the_question("#chosen_word_three_btn","#chosen_word_three_progress_bars_div .progress-bar-custom:first");
+			// 	answer_the_question("#chosen_word_four_btn","#chosen_word_four_progress_bars_div .progress-bar-custom:first");
+			//
+			// 	// Handles what happens when user answers correctly and incorrectly
+			// 	function answer_the_question(button_id_name, first_mini_progress_bar_without_pbc_class) {
+			// 		$(button_id_name).click(function(){
+			// 			// If the user's guess is correct
+			// 			if ($(this).text() == $words_array_five_each[$current_word_counter]) {
+			// 				// Update the nth mini progress bar for that word
+			// 				$(first_mini_progress_bar_without_pbc_class).removeClass("progress-bar-custom").addClass("progress-bar-success");
+			// 			}
+			//
+			// 			// If the user's guess is incorrect -->
+			// 			// Find the id of the correct word to update the nth progress bar as incorrect /
+			// 			if ($(this).text() != $words_array_five_each[$current_word_counter]) {
+			// 				$correct_word_proggress_bar_without_pbc_class = "#" +
+			// 						$("button:contains('"+$words_array_five_each[$current_word_counter]+"')")
+			// 						.next()
+			// 						.attr("id")
+			// 						+ " .progress-bar-custom:first"
+			// 					;
+			// 				$($correct_word_proggress_bar_without_pbc_class).removeClass("progress-bar-custom").addClass("progress-bar-danger");
+			// 			}
+			//
+			// 			// Total up the goodies
+			// 			get_goodies();
+			//
+			// 			// Shuffle the question types
+			// 			shuffle_array($question_type_array);
+			// 			// Increase the counter
+			// 			$current_word_counter++;
+			// 			console.log("Current word in the 20-word array (after clicked):",$words_array_five_each[$current_word_counter]);
+			// 			console.log("Randomly chosen question type (after clicked):",$question_type_array[0]);
+			// 			console.log("Question Types (after clicked):",$question_type_array[0]);
+			// 			console.log("Counter (after btn is clicked):",$current_word_counter);
+			// 			console.log("****************");
+			// 			display_activity_instruction(
+			// 				$question_type_array[0],
+			// 				$(xml).find($words_array_five_each[$current_word_counter]).children().find($question_type_array[0]+":first").text()
+			// 			);
+			//
+			// 			// When the game is over, display "complete" message
+			// 			if ($current_word_counter == 20) {
+			// 				$("#chosen_word_one_btn").prop("disabled",true);
+			// 				$("#chosen_word_two_btn").prop("disabled",true);
+			// 				$("#chosen_word_three_btn").prop("disabled",true);
+			// 				$("#chosen_word_four_btn").prop("disabled",true);
+			// 				display_activity_instruction("Ready for the results?", "3...2...1...");
+			// 				setTimeout(function(){
+			// 					$("#level_2_container").hide();
+			// 					$("#review_level_two_container").fadeIn();
+			//
+			// 					display_level_2_results(
+			// 						"#chosen_word_one_progress_bars_div",
+			// 						"#level_two_results_first_circle",
+			// 						"#level_two_results_first_word",
+			// 						$first_word
+			// 					);
+			//
+			// 					display_level_2_results(
+			// 						"#chosen_word_two_progress_bars_div",
+			// 						"#level_two_results_second_circle",
+			// 						"#level_two_results_second_word",
+			// 						$second_word
+			// 					);
+			//
+			// 					display_level_2_results(
+			// 						"#chosen_word_three_progress_bars_div",
+			// 						"#level_two_results_third_circle",
+			// 						"#level_two_results_third_word",
+			// 						$third_word
+			// 					);
+			//
+			// 					display_level_2_results(
+			// 						"#chosen_word_four_progress_bars_div",
+			// 						"#level_two_results_fourth_circle",
+			// 						"#level_two_results_fourth_word",
+			// 						$fourth_word
+			// 					);
+			//
+			// 				}, 3000);
+			// 			}
+			// 		});
+			// 	}; // end of answer the question fn
 			} // end of the success parameter in the ajax fn
 		}); // end of the ajax function
 	}; // end of start level 2 fn
@@ -211,31 +195,17 @@ $(document).ready(function(){
 	};
 
 	// Find the text value of an input
-	function words_array_five_each_push(chosen_word_num) {
-		$words_array_five_each.push($(chosen_word_num).val());
+	function words_array_five_each_push(word_object) {
+		$words_array_five_each.push(word_object);
 	};
 
 	// Display the button-text for the chosen words
-	function update_button_text(chosen_word_num_btn, chosen_word_num) {
-		$(chosen_word_num_btn).text($(chosen_word_num).val());
+	function update_button_text(chosen_word_num_btn, word_object) {
+		$(chosen_word_num_btn).text(word_object.name);
 	};
 
 	// Global fn
-	// Reset individual input values
-	function reset_input_value(id_name) {
-		$("#"+id_name).val(function (){
-			return this.defaultValue;
-		});
-	};
 
-	// Global fn
-	// Hide and show elements
-	function hide_and_show_button(hide, show) {
-		$(hide).hide();
-		$(show).show();
-	};
-
-	// Global fn
 	//Change the activity name and specific directions depending on current state
 	function display_activity_instruction(activity_name, specific_instruction) {
 		$("#activity_name").html(activity_name);
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,10 @@
 class GamesController < ApplicationController
+  before_action :chosen_word
+
   def fundamentals
-    @word = Word.find(params[:word_id])
-    @synonyms = @word.synonyms if @word.has_synonyms?
-    @antonyms = @word.antonyms if @word.has_antonyms?
-    @real_world_examples = RealWorldExample.provide(@word.name)
+    @synonyms = chosen_word.synonyms if chosen_word.has_synonyms?
+    @antonyms = chosen_word.antonyms if chosen_word.has_antonyms?
+    @real_world_examples = RealWorldExample.provide(chosen_word.name)
 
     if current_user.has_words? && current_user.has_incomplete_fundamentals?
       @rand_word_id = current_user.incomplete_fundamentals.sample.word.id
@@ -11,9 +12,20 @@ class GamesController < ApplicationController
   end
 
   def jeopardy
-    @word = Word.find(params[:word_id])
+  end
+
+  def jeopardy_game_words
     @second_word = Word.all.sample
     @third_word = Word.all.sample
     @fourth_word = Word.all.sample
+    @jeopardy_words = [chosen_word, @second_word, @third_word, @fourth_word]
+
+    render json: @jeopardy_words
+  end
+
+  private
+
+  def chosen_word
+    @chosen_word = Word.find(params[:word_id])
   end
 end
