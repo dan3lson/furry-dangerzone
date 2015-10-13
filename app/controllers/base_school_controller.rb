a93787215c483717534ff2b5d85ce84ade1b0f24
Danelson Rosa <danelson.rosa@gmail.com>
2015-10-12 21:57:00 -0400

    Separates teacher views with namespaces/frontend.

---
+++ app/controllers/base_school_controller.rb
@@ -0,0 +1,24 @@
+class BaseSchoolController < ApplicationController
+	before_action :logged_in_user
+  before_action :correct_user
+
+	layout "school_application"
+
+	private
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Yikes! Please log in first to do that."
+
+			redirect_to login_url
+		end
+	end
+
+	def correct_user
+		unless current_user.is_teacher?
+			flash[:danger] = "Yikes! That\'s not something you can do."
+
+			redirect_to root_path
+		end
+	end
+end
--- app/controllers/teachers_controller.rb
+++
@@ -1,7 +0,0 @@
-class TeachersController < ApplicationController
-  def classrooms
-    @fs_class_one = User.fs_class_one
-    @fs_class_two = User.fs_class_two
-    @fs_students = @fs_class_one + @fs_class_two
-  end
-end
