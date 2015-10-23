465f0d1486fbd2af38a7a3b4cf0d1ce7a806ff01
Danelson Rosa <danelson.rosa@gmail.com>
2015-10-23 09:21:17 -0400

    Updates stats.rb to simplify and view Leksi engagement.

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -1,20 +1,50 @@
+# Number of Logins
+User.all.map(&:num_logins).inject(0, &:+)
+
 # Users Without Words
-User.all.map { |u| u.words.count == 0 }.keep_if { |v| v == true }.count
+User.select { |u| u.words.count == 0 }.count
 
 # Users With Words
-User.all.map { |u| u.words.count > 0 }.keep_if { |v| v == true }.count
+User.select { |u| u.words.count > 0 }.count
 
 # Users Without Tags
-User.all.map { |u| u.tags.count == 0 }.keep_if { |v| v == true }.count
+User.select { |u| u.tags.count == 0 }.count
 
 # Users With Tags
-User.all.map { |u| u.tags.count > 0 }.keep_if { |v| v == true }.count
+User.select { |u| u.tags.count > 0 }.count
 
 # Fundamentals Completed
-UserWord.all.map { |uw| uw.fundamental_completed? }.keep_if { |v| v == true }.count
+UserWord.select { |uw| uw.fundamental_completed? }.count
+
+# Num Fundamentals Played
+GameStat.select { |gs| gs.game_id == 1 }.map(&:num_played).inject(0, &:+)
 
 # Fundamentals In Progress
-UserWord.all.map { |uw| uw.fundamental_in_progress? }.keep_if { |v| v == true }.count
+UserWord.select { |uw| uw.fundamental_in_progress? }.count
 
 # Fundamentals Not Started
-UserWord.all.map { |uw| uw.fundamental_not_started? }.keep_if { |v| v == true }.count
+UserWord.select { |uw| uw.fundamental_not_started? }.count
+
+# Jeopardys Completed
+UserWord.select { |uw| uw.jeopardy_completed? }.count
+
+# Num Jeopardys Played
+GameStat.select { |gs| gs.game_id == 2 }.map(&:num_played).inject(0, &:+)
+
+# Jeopardys Not Started
+UserWord.select { |uw| uw.jeopardy_not_started? }.count
+
+# Num Jeopardys Won
+GameStat.sum(:num_jeop_won)
+
+# Num Jeopardys Lost
+GameStat.sum(:num_jeop_lost)
+
+# Freestyles Completed
+UserWord.select { |uw| uw.freestyle_completed? }.count
+
+# Num Freestyles Played
+GameStat.select { |gs| gs.game_id == 3 }.map(&:num_played).inject(0, &:+)
+
+# Freestyles Not Started
+UserWord.select { |uw| uw.freestyle_not_started? }.count
