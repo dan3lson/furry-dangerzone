a1e1e61521014f075baae4669fb024faa75fc736
Danelson Rosa <danelson.rosa@gmail.com>
2016-01-04 01:32:23 -0500

    Pretty much finishes get started feature.

--- app/models/game_stat.rb
+++ app/models/game_stat.rb
@@ -7,4 +7,26 @@ class GameStat < ActiveRecord::Base
   validates :num_played, presence: true
   validates :num_jeop_won, presence: true
   validates :num_jeop_lost, presence: true
+
+  # not tested
+  def self.update_user_word_game_stats(user, word, game_name, time_spent)
+    user_word = UserWord.find_by(user: user, word: word)
+    game = Game.find_by(name: game_name)
+    gs = GameStat.where(game: game, user_word: user_word).first_or_initialize
+    time_spent = time_spent.to_f.round(2)
+
+    gs.num_played += 1
+    gs.time_spent += time_spent
+
+    if gs.save
+      msg = "Success: GameStat #{gs.id} num_played and time_spent "
+
+      "#{msg} updated for UW #{user_word.id} -> #{user_word.word.name}."
+    else
+      msg = "ERROR: GameStat #{gs.id} num_played OR time_spent not "
+      msg_2 = "successfully updated for UW #{user_word.id} -> "
+
+      "#{msg} #{msg_2} #{user_word.word.name}."
+    end
+  end
 end
