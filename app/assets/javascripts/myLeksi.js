f91bd83befec2c8f10499d515cdcd0382bfdf1a2
Danelson Rosa <danelson.rosa@gmail.com>
2016-05-18 01:07:34 -0400

    Makes frontend updates on search. Fixes view more words bug.

--- app/assets/javascripts/myLeksi.js
+++ app/assets/javascripts/myLeksi.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+	var NUM_WORDS = $("#myLeksi-words").data("total");
+
 	$("#myLeksi-pagination").hide();
 
-	if ($("#myLeksi-words .panel-default").length + 1 > 15) {
+	if (NUM_WORDS > 15) {
 		var url = $('.pagination .next a').attr('href');
 		var $view_more_link = $("<a>", {
 			role: "button",
@@ -12,17 +14,23 @@ $(document).ready(function() {
 			text: "VIEW MORE WORDS"
 		});
 		$("#view-more-link-container").html($view_more_link);
-		$("#view-more-link").click(load_more_words);
 	}
 
+	$("#view-more-link-container").on(
+		"click",
+		"#view-more-link",
+		load_more_words
+	);
+
 	function load_more_words() {
-		var num_words = $("#myLeksi-words .panel-default").length;
-		var next_page = num_words / 15 + 1;
+		var previous_url = $('#view-more-link').attr('href');
+		var previous_page = parseInt(previous_url.split("=")[1]);
+		var next_page = previous_page + 1;
 		var last_page = $(".pagination li").length - 2;
 		var new_url = "/myLeksi?page=" + next_page;
 
-		$("#view-more-link-container").append($view_more_link);
 		$("#view-more-link").attr("href", new_url);
+		$("#view-more-link-container").html($view_more_link);
 		if (next_page == last_page) $("#view-more-link").hide();
 		spinner();
 	}
