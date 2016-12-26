6973faa3cf12bebe1637c75ca3abc9768bf66e3a
Danelson Rosa <danelson.rosa@gmail.com>
2016-12-26 12:01:01 -0500

    Removes drop_table :examples from migration.

--- db/migrate/20161224214504_create_examples.rb
+++ db/migrate/20161224214504_create_examples.rb
@@ -1,6 +1,5 @@
 class CreateExamples < ActiveRecord::Migration
   def change
-    drop_table :examples
     create_table :examples do |t|
       t.string :text, null: false
       t.integer :word_id, null: false
