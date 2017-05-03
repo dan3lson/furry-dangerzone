659d40b6719d15705e1f015b473d0f328c2fb1c6
Danelson Rosa <danelson.rosa@gmail.com>
2017-05-03 01:11:00 -0400

    Updates freestyles count for admin index page.

--- app/controllers/admin/freestyles_controller.rb
+++ app/controllers/admin/freestyles_controller.rb
@@ -1,6 +1,6 @@
 class Admin::FreestylesController < BaseAdminController
   def index
     @freestyles = Freestyle.unreviewed.latest.page(params[:page])
-    @freestyle_count = Freestyle.count
+    @freestyle_count = @freestyles.count
   end
 end
