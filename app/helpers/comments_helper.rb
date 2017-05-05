8b8eed39c1ad00956c4e9d6bb2f955b2dd057613
Danelson Rosa <danelson.rosa@gmail.com>
2017-05-05 00:30:50 -0400

    Makes minor changes to views.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,7 +2,7 @@ module CommentsHelper
   def redo_comment_options(word)
     [
       "Just repeating the word doesn't make this pass. Please describe in complete sentences how you experienced the word \'#{word}\' in your life.",
-      "Just repeating the words doesn't make this pass. Please read the directions for this activity and answer it correctly.",
+      "Don't just repeat the words; please read the directions for this activity and answer it correctly.",
       "Do not write random words or letters. Please read the directions for this activity and answer it correctly.",
       "Instead of writing what letters are in both words, create a short story, poem, song, etc. that includes both words."
     ]
--- app/models/freestyle.rb
+++ app/models/freestyle.rb
@@ -2,12 +2,12 @@ class Freestyle < ActiveRecord::Base
   belongs_to :user_word
   has_one :freestyle_sent_stem, dependent: :destroy
   has_one :sent_stem, through: :freestyle_sent_stem
-  has_one :freestyle_rel_word
-  has_one :freestyle_lek_tale
+  has_one :freestyle_rel_word, dependent: :destroy
+  has_one :freestyle_lek_tale, dependent: :destroy
   has_one :freestyle_desc_me, dependent: :destroy
   has_one :describe_me, through: :freestyle_desc_me
   has_one :game_stat_freestyle, dependent: :destroy
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :input, presence: true
   validates :status, presence: true
