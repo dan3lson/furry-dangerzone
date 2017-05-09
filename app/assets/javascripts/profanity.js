65622c5f5db4bcc9500703bdf2560b795c67d9a0
Danelson Rosa <danelson.rosa@gmail.com>
2017-05-08 23:46:41 -0400

    Adds profanity filter.

---
+++ app/assets/javascripts/profanity.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	$(".container").on("click", "#search-btn", function(e) {
+		var $search = $("input[type='search']");
+		var text = $search.val();
+		$(".alert").remove();
+
+		getBadWords().done(function(response) {
+			var badRegex = new RegExp("\\b(" + response.join("|") + ")\\b", "ig");
+
+			if (hasBadWords(text, badRegex)) {
+				var $alert = createElem("div", "alert alert-danger");
+				var $icon = createElem("i", "fa fa-warning");
+				$alert.append(
+					$icon,
+					" Don\'t use bad words; your Teacher may see what you just did."
+				);
+				$(".search-container").append($alert);
+			} else {
+				$.getScript("/search_results?search=" + text);
+			}
+		});
+	});
+
+	function hasBadWords(text, regex) {
+		var regexes = {};
+		var types = [];
+		regexes["bad-words"] = regex;
+
+		for (var type in regexes) {
+			if (regexes[type].test(text)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function getBadWords() {
+		return $.getJSON('/bad-words/en.json');
+	}
+
+	function createElem(elem, _class, _id) {
+		_class = _class || null;
+		_id = _id || null;
+		return $("<" + elem + ">", { class: _class, id: _id });
+	}
+});
