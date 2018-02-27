882ba89c506e50227e59323cc45213a1f00b1060
Danelson Rosa <danelson.rosa@gmail.com>
2018-02-26 22:32:44 -0500

    Removes old stats that caused a Prod error.

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -13,13 +13,5 @@ User.where.not(id: UserWord.select(:user_id).uniq).count
 User.select { |u| u.num_words > 0 }.count
 User.where(id: UserWord.select(:user_id).uniq).count
 
-# Users Without Tags
-User.select { |u| u.tags.count == 0 }.count
-User.where.not(id: UserTag.select(:user_id).uniq).count
-
-# Users With Tags
-User.select { |u| u.tags.count > 0 }.count
-User.where(id: UserTag.select(:user_id).uniq).count
-
 # Display latest feedback
 #Feedback.all.map { |f| "#{f.user.username} submitted \'#{f.description}\' #{helper.time_ago_in_words(f.created_at)} ago." }.each { |i| puts i }
