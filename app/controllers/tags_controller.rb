2e08e8fd75822e5d1e71da0e8f001bd27bdfb93d
Danelson Rosa <danelson.rosa@gmail.com>
2018-03-05 21:26:54 -0500

    WIP: creating sections page; added Section to each word on Notebook page.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,11 @@ class TagsController < ApplicationController
 	before_action :logged_in_user
 
 	def index
-		@user_words = current_user.user_words_with_tags
+		@user_words_ids = current_user.user_words_with_tags.pluck(:id)
+		@user_tag_ids = UserWordUserTag.where(user_word: @user_words_ids)
+																	 .pluck(:user_tag_id)
+																	 .uniq
+    @user_tags = UserTag.find(@user_tag_ids)
 	end
 
 	private
