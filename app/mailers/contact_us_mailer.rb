1e8af9db9482722aa0c70691dc99cf5f4accbcbc
Danelson Rosa <danelson.rosa@gmail.com>
2018-03-19 21:58:00 -0400

    Updates email field to go to danelson@leksi.education

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,6 +1,6 @@
 class ContactUsMailer < ApplicationMailer
 	def new_message(fields)
 		@fields = fields
-		mail to: "danelson.rosa@gmail.com", subject: "New Online Leksi Message"
+		mail to: "danelson@leksi.education", subject: "New Online Leksi Message"
 	end
 end
